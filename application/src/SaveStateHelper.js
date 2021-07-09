73abec67a769e599db8cbe72d27a49d000b5456a
bombe-to <tjaeger@clausohm.de>
2021-07-09 23:43:49 +0200

    work state

--- application/src/SaveStateHelper.js
+++ application/src/SaveStateHelper.js
@@ -1,42 +1,76 @@
 const path =  require ("path")
 const fs = require ("fs")
-
+const tar = require ("tar")
+const { promisify } = require('util');
 
 class SaveStateHelper {
 
-    constructor(worldName) {
-        let userBasePath= "AppData/LocalLow/Redbeet Interactive/Raft/User/";
-        let worldBasePath = "/World/";
-        let userHome = process.env.HOME;
-
+    constructor(worldDir, worldName, timeStamp, isOfTar) {
+        this.worldDir = worldDir;
         this.worldName = worldName;
-        this.worldDir = fromDir(
-            path.join(
-                path.join(
-                    fromDir(
-                        path.join(
-                            userHome,
-                            userBasePath),
-                        "User_"),
-                    "World"),
-                worldName),
-            "-Latest");
+        this.timeStamp = timeStamp;
+        this.isOfTar = isOfTar;
+    }
 
-        const stats = fs.statSync('file.txt');
+    static ofTar(filePath){
+        let fileNameTime = path.basename(filePath).split("_");
+        let timeStemp = fileNameTime[fileNameTime.length-1];
 
-        // print file last modified date
+        let fileName = fileNameTime[0];
+        return new SaveStateHelper(extracted(fileName), fileName, timeStemp, true);
+    }
+
+    static ofWorld(worldName){
+        let worldDir = extracted(worldName);
+
+        const stats = fs.statSync(worldDir);
+        let timeStamp = stats.mtime;
         console.log(`File Data Last Modified: ${stats.mtime}`);
         console.log(`File Status Last Modified: ${stats.ctime}`);
+
+        return new SaveStateHelper(worldDir, worldName, timeStamp, false);
     }
+
 
     getTimeStamp(){
         return this.timeStamp;
     }
 
-    saveAsTar(){
+    saveAsTar(destinationPath){
+        promisify(tar.create)(
+            {
+                gzip: true,
+                file: this.worldName +  + ".tar",
+                cwd: path.resolve(path.dirname(this.worldDir)),
 
+            },
+            [this.worldDir]
+        )
     }
 
+    saveAsWorld(destinationPath){
+        fs.copy(this.worldDir, destinationPath);
+    }
+
+}
+
+function extracted(worldName) {
+    let userBasePath = "AppData/LocalLow/Redbeet Interactive/Raft/User/";
+    let worldBasePath = "/World/";
+    let userHome = process.env.HOME;
+
+    let worldDir = fromDir(
+        path.join(
+            path.join(
+                fromDir(
+                    path.join(
+                        userHome,
+                        userBasePath),
+                    "User_"),
+                "World"),
+            worldName),
+        "-Latest");
+    return worldDir;
 }
 
 function fromDir(startPath,filter){
--- application/test/SaveStateHelperTest.js
+++ application/test/SaveStateHelperTest.js
@@ -1,6 +1,15 @@
-const SaveStateHelper = require("../src/SaveStateHelper")
+const tmp = require('tmp');
+const tmpobj = tmp.fileSync();
+tmpobj.removeCallback();
 
-let was = new SaveStateHelper("GANDILEBT");
+const SaveStateHelper = require("../src/SaveStateHelper")
+let was = SaveStateHelper.ofWorld("GANDILEBT");
+
+let time = was.getTimeStamp();
+was.saveAsTar("")
+
+let wa2s = SaveStateHelper.ofTar("GANDILEBT");
+let time2 = was.getTimeStamp();
 
 
 let b = 0;
